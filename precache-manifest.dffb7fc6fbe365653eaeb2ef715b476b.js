7a18d95b0dfd401d0708d17e15b5976234c97a2b
ctcuff <dev.ctcuff@gmail.com>
2020-05-18 22:26:35 -0400

    Updates

---
+++ precache-manifest.dffb7fc6fbe365653eaeb2ef715b476b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc160f99296834e85281a00d910ba0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa1dd887107c5e6a5e7",
+    "url": "/static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "50f2f55e2945fa5d29ad",
+    "url": "/static/css/main.8b6aa8aa.chunk.css"
+  },
+  {
+    "revision": "8fa1dd887107c5e6a5e7",
+    "url": "/static/js/2.5b90f18f.chunk.js"
+  },
+  {
+    "revision": "2e1e2fdbc4f44105d2cb680fa918f632",
+    "url": "/static/js/2.5b90f18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f2f55e2945fa5d29ad",
+    "url": "/static/js/main.a358fc1b.chunk.js"
+  },
+  {
+    "revision": "79cf5dac26e69f2ec403",
+    "url": "/static/js/runtime-main.f9a1bd60.js"
+  },
+  {
+    "revision": "9cf0a1adee34b11d10d8c8d2e3e53438",
+    "url": "/static/media/404.9cf0a1ad.png"
+  }
+]);
