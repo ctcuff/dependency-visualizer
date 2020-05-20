7c41e681aa74de975f291cc2964b4dec93acc2f0
ctcuff <dev.ctcuff@gmail.com>
2020-05-20 13:51:26 -0400

    moved package info into its own component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,10 @@
 import './menu.scss'
 import React from 'react'
-import {
-    Layout,
-    Input,
-    Statistic,
-    Row,
-    Col,
-    Divider,
-    Button,
-    Tooltip,
-    Progress
-} from 'antd'
+import { Layout, Input, Statistic, Row, Col, Divider, Button } from 'antd'
 import { SearchOutlined, MenuOutlined, LeftOutlined } from '@ant-design/icons'
 import { connect } from 'react-redux'
 import { searchPackage } from '../../store/actions/search'
+import PackageInfo from '../PackageInfo'
 
 const { Sider } = Layout
 
@@ -27,7 +18,6 @@ class Menu extends React.Component {
 
         this.onProgressUpdate = this.onProgressUpdate.bind(this)
         this.onSearch = this.onSearch.bind(this)
-        this.renderPackageInfo = this.renderPackageInfo.bind(this)
         this.toggleMenu = this.toggleMenu.bind(this)
     }
 
@@ -49,116 +39,9 @@ class Menu extends React.Component {
         this.props.searchPackage(searchQuery)
     }
 
-    renderPackageInfo() {
-        if (!this.props.packageInfo) {
-            return null
-        }
-        const {
-            name,
-            description,
-            version,
-            dependencies,
-            downloads,
-            dependents,
-            stars,
-            forks,
-            quality,
-            popularity
-        } = this.props.packageInfo
-
-        const deps = Object.keys(dependencies)
-
-        return (
-            <React.Fragment>
-                <div className="package-info-block">
-                    <span className="label">Package</span>
-                    <div className="dependency-info package-name">
-                        <span className="dependency-name">{name}</span>
-                        <span>{version}</span>
-                    </div>
-                </div>
-                {description ? (
-                    <div className="package-info-block">
-                        <span className="label">Description</span>
-                        <p className="package-info">{description}</p>
-                    </div>
-                ) : null}
-                {stars && forks ? (
-                    <div className="package-info-block">
-                        <span className="label">Statistics</span>
-                        <Row className="stats" justify="center" align="center">
-                            <Col span={12}>
-                                <Statistic title="Stars" value={stars} />
-                            </Col>
-                            <Col span={12}>
-                                <Statistic title="Forks" value={forks} />
-                            </Col>
-                        </Row>
-                    </div>
-                ) : null}
-                <div className="package-info-block">
-                    <Row className="stats" justify="center" align="center">
-                        <Col span={12}>
-                            <Statistic title="Dependents" value={dependents} />
-                        </Col>
-                        <Col span={12}>
-                            <Statistic title="Downloads" value={downloads} />
-                        </Col>
-                    </Row>
-                </div>
-                <div className="package-info-block">
-                    <Row className="stats" justify="center" align="center" gutter={32}>
-                        <Col span={12}>
-                            <div className="stats-score">
-                                <span className="label">Quality</span>
-                                <Progress
-                                    type="circle"
-                                    size="small"
-                                    percent={quality}
-                                    className="stats-progress"
-                                />
-                            </div>
-                        </Col>
-                        <Col span={12}>
-                            <div className="stats-score">
-                                <span className="label">Popularity</span>
-                                <Progress
-                                    type="circle"
-                                    size="small"
-                                    percent={popularity}
-                                    className="stats-progress"
-                                />
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-                <div className="package-info-block">
-                    <span className="label">Direct Dependencies: {deps.length}</span>
-                    <div className="package-info">
-                        {deps.map(key => (
-                            <Tooltip
-                                placement="topLeft"
-                                title={`search ${key}`}
-                                key={key}
-                            >
-                                <div
-                                    className="dependency-info"
-                                    onClick={() => this.props.searchPackage(key)}
-                                >
-                                    <span className="dependency-name">{key}</span>
-                                    <span>{dependencies[key]}</span>
-                                </div>
-                            </Tooltip>
-                        ))}
-                    </div>
-                </div>
-            </React.Fragment>
-        )
-    }
-
     render() {
         return (
-            <div>
+            <div className="menu">
                 {this.state.isOpen ? (
                     <Button
                         className="menu-open-btn"
@@ -172,7 +55,7 @@ class Menu extends React.Component {
                     theme="light"
                     collapsedWidth={0}
                     collapsed={this.state.isOpen}
-                    className="menu"
+                    className="sider"
                     width="22em"
                 >
                     <Button
@@ -209,7 +92,10 @@ class Menu extends React.Component {
                             </Col>
                         </Row>
                         <Divider type="horizontal" className="divider" />
-                        {this.renderPackageInfo()}
+                        <PackageInfo
+                            packageInfo={this.props.packageInfo}
+                            onDependencyClick={this.props.searchPackage}
+                        />
                     </div>
                 </Sider>
             </div>
---
+++ src/components/PackageInfo/index.js
@@ -0,0 +1,107 @@
+import './package-info.scss'
+import React from 'react'
+import { Row, Col, Progress, Statistic } from 'antd'
+
+const PackageInfo = props => {
+    if (!props.packageInfo) {
+        return null
+    }
+    const {
+        name,
+        description,
+        version,
+        dependencies,
+        downloads,
+        dependents,
+        stars,
+        forks,
+        quality,
+        popularity
+    } = props.packageInfo
+
+    const deps = Object.keys(dependencies)
+
+    return (
+        <div className="package-info">
+            <div className="package-info-block">
+                <span className="label">Package</span>
+                <div className="dependency-info package-name">
+                    <span className="dependency-name">{name}</span>
+                    <span>{version}</span>
+                </div>
+            </div>
+            {description ? (
+                <div className="package-info-block">
+                    <span className="label">Description</span>
+                    <p className="package-description">{description}</p>
+                </div>
+            ) : null}
+            {stars && forks ? (
+                <div className="package-info-block">
+                    <span className="label">Statistics</span>
+                    <Row className="stats" justify="center" align="center">
+                        <Col span={12}>
+                            <Statistic title="Stars" value={stars} />
+                        </Col>
+                        <Col span={12}>
+                            <Statistic title="Forks" value={forks} />
+                        </Col>
+                    </Row>
+                </div>
+            ) : null}
+            <div className="package-info-block">
+                <Row className="stats" justify="center" align="center">
+                    <Col span={12}>
+                        <Statistic title="Dependents" value={dependents} />
+                    </Col>
+                    <Col span={12}>
+                        <Statistic title="Downloads" value={downloads} />
+                    </Col>
+                </Row>
+            </div>
+            <div className="package-info-block">
+                <Row className="stats" justify="center" align="center" gutter={32}>
+                    <Col span={12}>
+                        <div className="stats-score">
+                            <span className="label">Quality</span>
+                            <Progress
+                                type="circle"
+                                size="small"
+                                percent={quality}
+                                className="stats-progress"
+                            />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div className="stats-score">
+                            <span className="label">Popularity</span>
+                            <Progress
+                                type="circle"
+                                size="small"
+                                percent={popularity}
+                                className="stats-progress"
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <div className="package-info-block">
+                <span className="label">Direct Dependencies: {deps.length}</span>
+                <div className="dependencies">
+                    {deps.map(key => (
+                        <div
+                            key={key}
+                            className="dependency-info"
+                            onClick={() => props.onDependencyClick(key)}
+                        >
+                            <span className="dependency-name">{key}</span>
+                            <span>{dependencies[key]}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PackageInfo
